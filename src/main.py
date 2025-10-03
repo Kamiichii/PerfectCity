f72f7ea04e8425953babe604b646a7abbeb9f7d1
kamiichii <berkay.kilic.ce@gmail.com>
2025-10-03 17:11:19 +0300

    Made it so results are numbered and colored

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from input import input_and_calculate,replay_question
 from Attributes import Attributes
-
+from colorama import Fore,Style,init
 
 
 def main():
@@ -13,8 +13,15 @@ def main():
       input_and_calculate(Attributes.POPULATION,city_scores_dict)
       input_and_calculate(Attributes.CONTINENT,city_scores_dict)
       sorted_dict = dict(sorted(city_scores_dict.items(),key=lambda item: item[1]))
-      top10 =list(sorted_dict.keys())[:10]
-      print(f"Your top 10 city recommendations are: {top10}")
+      top10 =list(sorted_dict)[:10]
+      for i, city in enumerate(top10,start=1):
+         if i <= 3:
+            color = Fore.GREEN
+         elif i <= 7:
+               color = Fore.YELLOW
+         else:
+               color = Fore.RED
+         print(f"{i}. {color}{city} {Style.RESET_ALL}")
       replay = replay_question()
 
    
